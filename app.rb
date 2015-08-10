09247d50794109264219bc1317f0ac5ca78bc27e
Tim Kellogg & Josue Valverde <student@epicodus.com>
2015-08-10 10:23:06 -0700

    add route and feed to method

--- app.rb
+++ app.rb
@@ -6,3 +6,14 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/results') do
+
+  side_one   = params.fetch('side_one')
+  side_two   = params.fetch('side_two')
+  side_three = params.fetch('side_three')
+
+  @triangle  = Triangle.new(side_one, side_two, side_three)
+  @type      = @triangle.type
+  erb(:results)
+end
