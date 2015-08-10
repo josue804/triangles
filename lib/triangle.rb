723ae18eba1a74d21a3373c978eb5dd3af86dda1
Tim Kellogg & Josue Valverde <student@epicodus.com>
2015-08-10 09:51:35 -0700

    add code that checks for a scalene triangle.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,6 +21,8 @@ class Triangle
       "Equilateral"
     elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
       "Isosceles"
+    else
+      "Scalene"
     end
   end
 end
