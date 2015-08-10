980beab9fb73dde9faa087d53b3cf1783980cba8
Tim Kellogg & Josue Valverde <student@epicodus.com>
2015-08-10 10:57:42 -0700

    add catch-all route

--- app.rb
+++ app.rb
@@ -13,7 +13,18 @@ get('/results') do
   side_two   = params.fetch('side_two')
   side_three = params.fetch('side_three')
 
-  @triangle  = Triangle.new(side_one, side_two, side_three)
-  @type      = @triangle.type
-  erb(:results)
+  @triangle  = Triangle.new(side_one.to_i(), side_two.to_i(), side_three.to_i())
+
+  if @triangle.triangle?()
+    @type      = @triangle.type()
+    erb(:results)
+  else
+    @message = "That's not a triangle, daft git"
+    erb(:index)
+  end
+end
+
+
+get('/*') do
+  erb(:index)
 end
